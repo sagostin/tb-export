3fea56ea27c6a650c94df0fc703484f35a202a3a
Shaun Agostinho <shaunagostinho@gmail.com>
2023-06-08 09:39:51 -0700

    outputting correct data on fetching metrics

--- exporter.go
+++ exporter.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/sagostin/tbgo/sbc"
@@ -9,6 +8,7 @@ import (
 	"reflect"
 	"strings"
 	"sync"
+	"time"
 )
 
 const (
@@ -18,51 +18,6 @@ const (
 func (e *Exporter) BuildDescriptions() {
 	// build map for descriptions that is limitless >:) (not really)
 	var metricDesc = make(map[string]*prometheus.Desc, 0)
-
-	/*log.Info("Loading fields for CallLeg status.")
-
-	// todo do i even need to use this old status stuff?
-	// can i convert to use just the direct pulling from the tbstatus??
-
-	// general status metric fields
-	status, err := e.client.TBStatus().GetStatus()
-	if err != nil {
-		log.Errorf("Can't query Service API: %v", err)
-		return
-	}
-
-	val := reflect.ValueOf(status.CallLegs)
-
-	var fields []string
-
-	for i := 0; i < val.Type().NumField(); i++ {
-		//fmt.Println(val.Type().Field(i).Tag.Get("json"))
-		fields = append(fields, val.Type().Field(i).Tag.Get("json"))
-	}
-
-	labels := []string{"id"}
-
-	clFields := fields
-	for _, i := range clFields {
-		log.Infof("Adding CallLeg field: %s", i)
-		newDesc := prometheus.NewDesc(
-			prometheus.BuildFQName(namespace, "status", i),
-			fmt.Sprintf("CallLeg field: %s", i),
-			labels, nil,
-		)
-
-		// for individual nap fields we will want to add a nap label before the field name
-		metricDesc[i] = newDesc
-	}*/
-
-	/*napStatus, err := e.client.TBNaps().GetNapStatus(e.config, nap)
-	if err != nil {
-		log.Errorf("Can't query Service API: %v", err)
-		return
-	}
-
-	var nStatus sbc.NapStatus
-	nStatus = *napStatus*/
 
 	tempNapStatusFormat := sbc.NapStatus{}
 
@@ -74,10 +29,10 @@ func (e *Exporter) BuildDescriptions() {
 		//fmt.Println(valFirst.Type().Field(i).Tag.Get("json"))
 		nF1 := strings.Replace(valFirst.Type().Field(i).Tag.Get("json"), ",omitempty", "", -1)
 		if valFirst.Type().Field(i).Type.Kind() == reflect.Struct {
-			//fmt.Println("Struct: ", valFirst.Type().Field(i).Type)
-			//fmt.Println("Struct: ", valFirst.Type().Field(i).Type.NumField())
+			fmt.Println("Struct 1: ", valFirst.Type().Field(i).Type)
+			fmt.Println("Struct 2: ", valFirst.Type().Field(i).Type.NumField())
 			for j := 0; j < valFirst.Type().Field(i).Type.NumField(); j++ {
-				//fmt.Println("Struct: ", valFirst.Type().Field(i).Type.Field(j).Tag.Get("json"))
+				fmt.Println("Struct 3: ", valFirst.Type().Field(i).Type.Field(j).Tag.Get("json"))
 				nF2 := strings.Replace(valFirst.Type().Field(i).Type.Field(j).Tag.Get("json"), ",omitempty", "", -1)
 				napFields = append(napFields, nF1+"__"+nF2)
 			}
@@ -150,113 +105,64 @@ func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
 }
 
 func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
-	nap, err := GetStatusNAP(e.tbCliStatus)
+	naps, err := GetStatusNAP(e.tbCliStatus)
 	if err != nil {
 		log.Errorf("Can't query Service API: %v", err)
 		return
 	}
-	var wg sync.WaitGroup
-
-	for k, v := range nap {
-		log.Warnf("NAP: %s", k)
-		marshal, err := json.Marshal(v)
-		if err != nil {
-			return
-		}
-		marshalString := string(marshal)
-		log.Warnf(marshalString)
-	}
-
-	log.Infoln("Waiting for all goroutines to finish")
-	wg.Wait()
-
-	// get status
-	/*status, err := e.client.TBStatus().GetStatus()
-
-	if err != nil {
-		log.Errorf("Can't query Service API: %v", err)
-		return
-	}
-
-	val := reflect.ValueOf(status.CallLegs)
-
-	for i := 0; i < val.Type().NumField(); i++ {
-		field := val.Field(i)
-		fieldName := val.Type().Field(i).Tag.Get("json")
-		if field.Kind() == reflect.Int {
-			ch <- prometheus.NewMetricWithTimestamp(time.Now(), prometheus.MustNewConstMetric(e.desc[fieldName], prometheus.GaugeValue, float64(field.Int()), e.id))
-		} else if field.Kind() == reflect.Float64 {
-			ch <- prometheus.NewMetricWithTimestamp(time.Now(), prometheus.MustNewConstMetric(e.desc[fieldName], prometheus.GaugeValue, field.Float(), e.id))
-		}
-	}*/
-
-	/*// Create a rate limiter
-	limiter := rate.NewLimiter(rate.Limit(maxRequests), burst)
 
 	// todo make this more efficient
-	// get naps, and load individual statistics
-	naps, err := e.client.TBNaps().GetNames(e.config)
-	if err != nil {
-		log.Errorf("Can't query Service API: %v", err)
-		return
-	}
+	// get naps, and load individual statistics for each nap
 	var wg sync.WaitGroup
-	for _, nap := range naps {
-		if nap == "" {
-			log.Infoln("Nap is empty, skipping")
-			continue
-		}
-
-		// Apply rate limiting before making each API request
-		if err := limiter.Wait(context.Background()); err != nil {
-			log.Errorf("Rate limit exceeded: %v", err)
-			time.Sleep(minDelay) // Wait for the minimum delay before retrying
-			continue
-		}
+	for n, nap := range naps {
 		wg.Add(1)
-		go func(cCh chan<- prometheus.Metric, n string, client sbc.Client, config string) {
+		go func(cCh chan<- prometheus.Metric, nStatus *NapStatus, nap string, client sbc.Client, config string) {
 			defer wg.Done()
-			napStatus, err := client.TBNaps().GetNapStatus(config, n)
-			if err != nil {
-				log.Errorf("Can't query Service API: %v", err)
-				return
-			}
-			var nStatus sbc.NapStatus
-			nStatus = *napStatus
+			nVal := reflect.ValueOf(nStatus).Elem()
 
-			nVal := reflect.ValueOf(nStatus)
-
-			for i := 0; i < nVal.Type().NumField(); i++ {
-				field := nVal.Field(i)
+			for i := 0; i < nVal.NumField(); i++ {
+				field := nVal.Type().Field(i)
 
 				// remove omitempty from json tag
 				fieldName := strings.Replace(nVal.Type().Field(i).Tag.Get("json"), ",omitempty", "", -1)
-				if field.Kind() == reflect.Int {
+				if field.Type.Kind() == reflect.Int {
 					log.Infoln("NAP field: ", n, fieldName)
 					//e.desc[n+"-"+fieldName]
-					cCh <- prometheus.NewMetricWithTimestamp(time.Now(), prometheus.MustNewConstMetric(e.desc[fieldName], prometheus.GaugeValue, float64(field.Int()), n, e.id))
-				} else if field.Kind() == reflect.Float64 {
+					cCh <- prometheus.NewMetricWithTimestamp(time.Now(),
+						prometheus.MustNewConstMetric(e.desc[fieldName],
+							prometheus.GaugeValue, float64(nVal.Field(i).Int()), nap, e.id))
+				} else if field.Type.Kind() == reflect.Float64 {
 					log.Infoln("NAP field: ", n, fieldName)
-					cCh <- prometheus.NewMetricWithTimestamp(time.Now(), prometheus.MustNewConstMetric(e.desc[fieldName], prometheus.GaugeValue, field.Float(), n, e.id))
-				} else if field.Kind() == reflect.Struct {
+					cCh <- prometheus.NewMetricWithTimestamp(time.Now(),
+						prometheus.MustNewConstMetric(e.desc[fieldName],
+							prometheus.GaugeValue, nVal.Field(i).Float(), nap, e.id))
+				} else if field.Type.Kind() == reflect.Struct {
 					log.Infoln("NAP field: ", n, fieldName)
-					for i2 := 0; i2 < field.NumField(); i++ {
-						field2 := field.Field(i2)
-						fieldName2 := field.Type().Field(i2).Tag.Get("json")
-						if field2.Kind() == reflect.Int {
-							cCh <- prometheus.NewMetricWithTimestamp(time.Now(), prometheus.MustNewConstMetric(e.desc[fieldName+"__"+fieldName2], prometheus.GaugeValue, float64(field2.Int()), n, e.id))
-						} else if field2.Kind() == reflect.Float64 {
-							cCh <- prometheus.NewMetricWithTimestamp(time.Now(), prometheus.MustNewConstMetric(e.desc[fieldName+"__"+fieldName2], prometheus.GaugeValue, field2.Float(), n, e.id))
+					log.Warnf("Struct: %s", fieldName)
+					vVal := nVal.Field(i)
+					for i2 := 0; i2 < vVal.NumField(); i2++ {
+						field2 := vVal.Type().Field(i2)
+						fieldName2 := field2.Tag.Get("json")
+						if field2.Type.Kind() == reflect.Int {
+							cCh <- prometheus.NewMetricWithTimestamp(time.Now(),
+								prometheus.MustNewConstMetric(e.desc[fieldName+"__"+fieldName2],
+									prometheus.GaugeValue, float64(vVal.Field(i2).Int()), nap, e.id))
+						} else if field2.Type.Kind() == reflect.Float64 {
+							cCh <- prometheus.NewMetricWithTimestamp(time.Now(),
+								prometheus.MustNewConstMetric(e.desc[fieldName+"__"+fieldName2],
+									prometheus.GaugeValue, vVal.Field(i2).Float(), nap, e.id))
+						} else {
+							log.Infoln("NAP field: ", n, fieldName, fieldName2)
+							continue
 						}
 					}
 				} else {
-					log.Errorf("Unknown field type: %s", fieldName)
+					log.Errorf("Unknown field type: %s", field.Type.Kind())
+					continue
 				}
 			}
-		}(ch, nap, e.client, e.config)
-		// cycle through the naps, then initialize a go func for each one running in the background,
-		// when that func is complete, had it send to channel, and same for the others
+		}(ch, nap, n, e.client, e.config)
 	}
 	wg.Wait()
-	log.Infoln("Done collecting metrics")*/
+	log.Infoln("Done collecting nap metrics")
 }
